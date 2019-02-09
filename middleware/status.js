51c0fce1014f56ac18f4313b7f32af7a5cfc197d
云游君 <me@yunyoujun.cn>
2019-02-09 20:24:03 +0800

    fix header token

--- middleware/status.js
+++ middleware/status.js
@@ -3,18 +3,11 @@ export default function({ $axios, store }) {
     if (localStorage.getItem('sessionToken') !== null) {
       let sessionToken = localStorage.getItem('sessionToken')
       store.commit('SET_SESSION_TOKEN', sessionToken)
+      store.commit('SET_USER', localStorage.getItem('username'))
+      store.commit('SET_OBJECT_ID', localStorage.getItem('objectId'))
     }
   }
-  if (!store.state.username) {
-    if (localStorage.getItem('username') !== null) {
-      let username = localStorage.getItem('username')
-      store.commit('SET_USER', username)
-    }
-  }
-  if (!store.state.objectId) {
-    if (localStorage.getItem('objectId') !== null) {
-      let objectId = localStorage.getItem('objectId')
-      store.commit('SET_OBJECT_ID', objectId)
-    }
+  if (store.state.sessionToken) {
+    $axios.setHeader('X-LC-Session', store.state.sessionToken)
   }
 }
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,6 @@
 export default function({ $axios, store }) {
   $axios.setHeader('X-LC-Id', 'SqGXqvMqLFSGgxYpcXFbmBgR-gzGzoHsz')
   $axios.setHeader('X-LC-Key', 'kUXrbjyUdvJV4i1FhsVayqa4')
-  if (store.state.sessionToken) {
-    $axios.setHeader('X-LC-Session', sessionToken)
-  }
   $axios.onRequest(config => {
     console.log('Making request to ' + config.url)
   })
